ed194d5a2bf1e9c9ace542f89e4b16db0758d3d0
tamashiro-syuta <syuta5825@gmail.com>
2022-05-14 13:59:44 +0900

    それぞれのcomponentを表示できるようにした

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TitleForm from './component/TitleForm';
+import AddButton from './component/AddButton';
+import TodoList from './component/TodoList';
+import SearchForm from './component/SearchForm';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ margin: 12 }}>
+      <div>
+        <TitleForm />
+        <AddButton />
+      </div>
+      <SearchForm />
+      <TodoList />
     </div>
   );
 }
