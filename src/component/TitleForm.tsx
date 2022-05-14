0ca798c779a03f30f81cfebc8c17eabc4a3a27bc
tamashiro-syuta <syuta5825@gmail.com>
2022-05-14 13:58:29 +0900

    componentの作成

---
+++ src/component/TitleForm.tsx
@@ -0,0 +1,42 @@
+import { useCallback } from "react";
+import { useRecoilValue, useSetRecoilState, SetterOrUpdater } from "recoil";
+import todoTitleFormState from "../atoms/TodoTitleFromAtom";
+
+
+// reducやmobxと同様に、RecoilにおいてもAtomの値を変更することで、参照しているSelectorやコンポーネントに自動反映されます。
+// Atomの値を変更するためにはuseSetRecoilState()にAtomを引数として渡します。
+// 返り値としてSetterOrUpdater<T>型の関数が返されます。
+// これはAtomの値に対するSetter関数のようなもので、この関数を経由してAtomを更新することで前述の再計算処理が行われます。
+
+
+const TitleForm: React.FC = () => {
+  // useRecoilValueでtodoTitleFormStateの値を取得
+  const todoTitleFormValue: string = useRecoilValue(todoTitleFormState);
+  // useSetRecoilStateでtodoTitleFormStateの値を更新するSetter関数を取得
+  const setTodoTitleFormValue: SetterOrUpdater<string> = useSetRecoilState(
+    todoTitleFormState
+  );
+  // テキストフィールドのonChange処理
+  const onChange = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      // 先に取得したsetTodoTitleFormValueに対して更新したい値を渡して実行
+      setTodoTitleFormValue(event.target.value);
+    },
+    [setTodoTitleFormValue]
+  );
+
+  return (
+    <label>
+      タスク名:
+      <input
+        type="text"
+        value={todoTitleFormValue}
+        onChange={onChange}
+        name="title"
+        style={{ margin: 12 }}
+      />
+    </label>
+  );
+};
+
+export default TitleForm;
---
+++ src/component/TodoList.tsx
@@ -0,0 +1,22 @@
+// 値を取得するには、useRecoilValue を使う
+import { useRecoilValue } from "recoil";
+import searchedTodoListSelector  from '../selectors/SearchedTodoListSelector'
+import Todo from "../types/Todo";
+
+const TodoList: React.FC = () => {
+  // useRecoilValueにsearchedTodoListSelectorの値を渡してる
+  // 返される値は、searchedTodoListSelectorのget()で定義した通りTodo[]
+  const list: Todo[] = useRecoilValue(searchedTodoListSelector);
+  return (
+    <div>
+      <p>タスク一覧</p>
+      <ul>
+        {list.map((todo: Todo, i: number) => {
+          return <li key={`${todo.title}_${i}`}>{todo.title}</li>;
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default TodoList;
